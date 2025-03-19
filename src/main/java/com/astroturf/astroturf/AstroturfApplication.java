bc088fbd025b73937ed801815b6cbb55f46e2870
BradTheeStallion <bradley.ayers@keyin.com>
2025-03-19 08:20:32 -0230

    Initial commit

---
+++ src/main/java/com/astroturf/astroturf/AstroturfApplication.java
@@ -0,0 +1,13 @@
+package com.astroturf.astroturf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AstroturfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AstroturfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/astroturf/astroturf/AstroturfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.astroturf.astroturf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AstroturfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
