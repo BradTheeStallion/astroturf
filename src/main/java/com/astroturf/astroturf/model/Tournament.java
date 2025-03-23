785253839b6071ae94dffac4cb6dcc40aff71ce8
BradTheeStallion <bradley.ayers@keyin.com>
2025-03-23 10:14:22 -0230

    Restoring previous versions of files

--- src/main/java/com/astroturf/astroturf/model/Tournament.java
+++ src/main/java/com/astroturf/astroturf/model/Tournament.java
@@ -1,19 +1,13 @@
 package com.astroturf.astroturf.model;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 @Entity
-@Data
-@NoArgsConstructor
-@AllArgsConstructor
 @Table(name = "tournaments")
 public class Tournament {
 
@@ -43,4 +37,111 @@ public class Tournament {
             inverseJoinColumns = @JoinColumn(name = "member_id")
     )
     private Set<Member> participants = new HashSet<>();
+
+    // Default constructor
+    public Tournament() {
+    }
+
+    // All-args constructor
+    public Tournament(Long id, LocalDate startDate, LocalDate endDate, String location,
+                      BigDecimal entryFee, BigDecimal cashPrize, Set<Member> participants) {
+        this.id = id;
+        this.startDate = startDate;
+        this.endDate = endDate;
+        this.location = location;
+        this.entryFee = entryFee;
+        this.cashPrize = cashPrize;
+        this.participants = participants != null ? participants : new HashSet<>();
+    }
+
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDate getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(LocalDate startDate) {
+        this.startDate = startDate;
+    }
+
+    public LocalDate getEndDate() {
+        return endDate;
+    }
+
+    public void setEndDate(LocalDate endDate) {
+        this.endDate = endDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public BigDecimal getEntryFee() {
+        return entryFee;
+    }
+
+    public void setEntryFee(BigDecimal entryFee) {
+        this.entryFee = entryFee;
+    }
+
+    public BigDecimal getCashPrize() {
+        return cashPrize;
+    }
+
+    public void setCashPrize(BigDecimal cashPrize) {
+        this.cashPrize = cashPrize;
+    }
+
+    public Set<Member> getParticipants() {
+        return participants;
+    }
+
+    public void setParticipants(Set<Member> participants) {
+        this.participants = participants;
+    }
+
+    // equals method
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Tournament that = (Tournament) o;
+        return Objects.equals(id, that.id) &&
+                Objects.equals(startDate, that.startDate) &&
+                Objects.equals(endDate, that.endDate) &&
+                Objects.equals(location, that.location) &&
+                Objects.equals(entryFee, that.entryFee) &&
+                Objects.equals(cashPrize, that.cashPrize);
+    }
+
+    // hashCode method
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, startDate, endDate, location, entryFee, cashPrize);
+    }
+
+    // toString method
+    @Override
+    public String toString() {
+        return "Tournament{" +
+                "id=" + id +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", location='" + location + '\'' +
+                ", entryFee=" + entryFee +
+                ", cashPrize=" + cashPrize +
+                ", participants=" + participants +
+                '}';
+    }
 }
